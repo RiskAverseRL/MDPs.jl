e02338974bd4f452c132051596daf0fe7b450883
Keith Badger <keithbadger699@gmail.com>
2024-07-09 17:06:49 -0400

    changed linear_program_solve to require gamma as float rather than enclose it in objective object

--- src/algorithms/linprogsolve.jl
+++ src/algorithms/linprogsolve.jl
@@ -4,7 +4,8 @@ using HiGHS, JuMP
 Implments the linear programming method of solving an MDP "model" with an infinite horizon and discount factor γ. The function utilizes the HiGHS optimizer which is free to use.
 """
 
-function linear_program_solve(model::TabMDP, objective::Stationary)
+function linear_program_solve(model::TabMDP, γ::Float)
+    0 ≤ γ < 1 || error("γ must be between 0 and 1")
     lpm = Model(HiGHS.Optimizer)
     set_silent(lpm)
     n = state_count(model)
@@ -14,7 +15,7 @@ function linear_program_solve(model::TabMDP, objective::Stationary)
         m = action_count(model,s)
         for a in 1:m
             snext = transition(model,s,a)
-            @constraint(lpm, v[s] ≥ sum(sp[2]*(sp[3]+objective.γ*v[sp[1]]) for sp in snext))
+            @constraint(lpm, v[s] ≥ sum(sp[2]*(sp[3]+γ*v[sp[1]]) for sp in snext))
         end
     end
     optimize!(lpm)
