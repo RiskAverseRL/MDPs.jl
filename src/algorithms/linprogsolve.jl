61d52c31bba0b327ac905b1de92f7d90dcdeefba
Keith Badger <keithbadger699@gmail.com>
2024-07-10 11:29:09 -0400

    fixed linprogsolve output and added optimizer options

--- src/algorithms/linprogsolve.jl
+++ src/algorithms/linprogsolve.jl
@@ -1,12 +1,13 @@
-using HiGHS, JuMP
+using JuMP
 
 """
-Implments the linear programming method of solving an MDP "model" with an infinite horizon and discount factor γ. The function utilizes the HiGHS optimizer which is free to use.
+Implments the linear programming method of solving an MDP "model" with an infinite horizon and discount factor γ.
+The function utilizes the HiGHS optimizer which is free to use.
 """
 
-function linear_program_solve(model::TabMDP, γ::Number)
-    0 ≤ γ < 1 || error("γ must be between 0 and 1")
-    lpm = Model(HiGHS.Optimizer)
+
+function linear_program_solve(model::TabMDP, objective::InfiniteH, optimizer)
+    lpm = Model(optimizer)
     set_silent(lpm)
     n = state_count(model)
     @variable(lpm, v[1:n])
@@ -15,9 +16,9 @@ function linear_program_solve(model::TabMDP, γ::Number)
         m = action_count(model,s)
         for a in 1:m
             snext = transition(model,s,a)
-            @constraint(lpm, v[s] ≥ sum(sp[2]*(sp[3]+γ*v[sp[1]]) for sp in snext))
+            @constraint(lpm, v[s] ≥ sum(sp[2]*(sp[3]+objective.γ*v[sp[1]]) for sp in snext))
         end
     end
     optimize!(lpm)
-    return [value(v[k]) for k in 1:n]
+    return value.(v)
 end
