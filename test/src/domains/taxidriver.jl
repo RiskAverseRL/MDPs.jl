1720d657ba7fa4c896b6f40f9aaee35f061c5aab
Ahmed Senior Ismail <cnior4real@gmail.com>
2024-07-27 06:54:32 -0400

    a simple case for testset

--- test/src/domains/taxidriver.jl
+++ test/src/domains/taxidriver.jl
@@ -1,11 +1,11 @@
 using MDPs.Domains
 
 @testset "Solve TaxiDriver.Taxi" begin
-    pickup_rates = [.9,.3,.1]
-    destination_prob = [0.0 .8 .2; .6 0.0 .4; .1 .9 0.0]
-    transition_cost = [0.0 1.5 2.0; 3.0 0.0 1.0; 2.0 1.5 0.0]
-    transition_profit = [0.0 8.5 10.0; 9.0 0.0 12.0; 10.0 7.0 0.0]
-    num_locs = 3
+    pickup_rates = [1.0, 0.0] #[.9,.3,.1]
+    destination_prob = [0.0 1.0;1.0 0.0] #[0.0 .8 .2; .6 0.0 .4; .1 .9 0.0]
+    transition_cost = [0.0 1.0;1.0 0.0] #[0.0 1.5 2.0; 3.0 0.0 1.0; 2.0 1.5 0.0]
+    transition_profit = [0.0 2.0;1.0 0.0] #[0.0 8.5 10.0; 9.0 0.0 12.0; 10.0 7.0 0.0]
+    num_locs = 2
 
     model = TaxiDriver.Taxi(pickup_rates, destination_prob,transition_cost, transition_profit,num_locs )
     simulate(model, random_π(model), 1, 10000, 500)
