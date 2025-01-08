6d424adcc0f5d7a8986747f6a09868ed0bc55203
Gersi.doko <Gersi.doko@unh.edu>
2025-01-08 17:02:27 -0500

    fix tests and docs

--- test/src/simulate.jl
+++ test/src/simulate.jl
@@ -2,9 +2,7 @@ using MDPs
 using MDPs.Domains
 
 
-@test "basic simulation" begin
-
-    
+@testset "basic simulation" begin
     model = Domains.Gambler.Ruin(0.7, 10)
     initstate = 1
     horizon = 100
@@ -12,16 +10,17 @@ using MDPs.Domains
 
     π = ones(Int, state_count(model)) * 2
     π[1] = 1
-    
+    π[end] = 1
+
     # simulate a single initial state
     inistate = 3
     H = simulate(model, π, inistate, horizon, episodes)
-    @test all(H.states[1,:] .== 3)
+    @test all(H.states[1, :] .== 3)
 
     # simulate a distribution
     initial = ones(state_count(model))
     initial[end] = 0
     initial /= sum(initial)
     H = simulate(model, π, initial, horizon, episodes)
-    @test length(unique(H.states[1,:])) ≥ 3
+    @test length(unique(H.states[1, :])) ≥ 3
 end
